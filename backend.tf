cb6675f0e418ab37841a7b3f4ebd62e17b167f54
Jules Martin <julesmartin76000@gmail.com>
2022-02-08 14:49:03 +0100

    Add Kapsule cluster

--- backend.tf
+++ backend.tf
@@ -13,13 +13,10 @@ terraform {
 
 /*
 
-For the credentials part, 2 methods:
-1 - Simply use 
-export TF_VAR_access_key=XXXXXXXX
-export TF_VAR_secret_key=XXXXX
+For the credentials part:
 
-2 - Otherwise
-Create a ~/.aws/credentials
+==> Create a ~/.aws/credentials:
+
 [default]
 aws_access_key_id=<SCW_ACCESS_KEY>
 aws_secret_access_key=<SCW_SECRET_KEY>
--- terraform.tfvars
+++ terraform.tfvars
@@ -2,3 +2,11 @@
 zone                           = "fr-par-1"
 region                         = "fr-par"
 env                            = "dev"
+
+
+# Kapsule
+kapsule_cluster_version        = "1.22"
+kapsule_pool_size              = 2
+kapsule_pool_min_size          = 2
+kapsule_pool_max_size          = 4
+kapsule_pool_node_type         = "DEV1-M"
